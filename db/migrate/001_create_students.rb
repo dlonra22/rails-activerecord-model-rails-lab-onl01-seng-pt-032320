cc81dc98a767a14dd6389ad1a40191517fde79de
arnie b <arnold.bwaila@gmail.com>
2020-08-13 15:28:27 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_students.rb
@@ -0,0 +1,8 @@
+class CreateStudents <ActiveRecord::Migration
+  change do 
+    create_table :students do |t| 
+      t.string :first_name
+      t.string :last_name 
+      t.datetime null: true 
+  
+end
